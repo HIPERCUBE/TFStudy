f9b28bdf8ed5489d08db105e3372f038c9b7e183
Joowon Ryoo <joowonryoo@gmail.com>
2016-04-22 21:45:02 +0900

    Update 01-Basics/01-helloword

--- 01-Basics/01-helloworld.py
+++ 01-Basics/01-helloworld.py
@@ -1,9 +1,15 @@
 import tensorflow as tf
 
+# Create hello world using TensorFlow
+# The op is added as node to the default graph.
+#
+# The value returned by the constructor represents the ouput
+# of the Constant op.
 hello = tf.constant('Hello, TensorFlow!')
 
 print hello
 
+# Start tf session
 sess = tf.Session()
 
 print sess.run(hello)
